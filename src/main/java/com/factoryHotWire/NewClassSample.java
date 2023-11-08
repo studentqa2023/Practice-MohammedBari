b476d4bed21e8107a2b2de992e3cdff1e3a85656
student2023 <mailto:rezwanislam857@gmail.com>
2023-11-07 19:25:27 -0600

    This is new addition

---
+++ src/main/java/com/factoryHotWire/NewClassSample.java
@@ -0,0 +1,14 @@
+package com.factoryHotWire;
+
+public class NewClassSample {
+
+	
+	public void grtNewClass() {
+		System.out.println("this is new sample class");
+	}
+	
+	public static void main(String[] args) {
+		NewClassSample obj = new NewClassSample();
+		obj.grtNewClass();
+	}
+}
